c523124ef6aeb8e0fcfdc65e3611ee74cc6ef7a2
Caleb Cox <canac@users.noreply.github.com>
2021-08-25 09:32:29 -0500

    Support adding new servers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use clap::{App, Arg, SubCommand};
 use dialoguer::{theme::ColorfulTheme, Select};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::fmt;
 use std::fs;
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -74,17 +76,86 @@ fn pick_server(config: &mut Config) -> &mut Server {
     &mut config.servers[index]
 }
 
+// Add a new server to the config
+fn add_server(config: &mut Config, project_name: &str) {
+    let package_json_path = format!("{}/{}/package.json", config.servers_dir, project_name);
+    let package_json_content =
+        fs::read_to_string(package_json_path).expect("Error reading package.json");
+    let package_json: Value = serde_json::from_str(&package_json_content).unwrap();
+    if let Value::Object(scripts) = &package_json["scripts"] {
+        let script_names = scripts.iter().map(|(key, _)| key).collect::<Vec<_>>();
+        let options = scripts
+            .iter()
+            .map(|(key, value)| format!("{}: {}", key, value))
+            .collect::<Vec<_>>();
+        let selected = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Pick a start command")
+            .default(0)
+            .items(&options)
+            .interact()
+            .unwrap();
+        config.servers.push(Server {
+            project_name: project_name.to_string(),
+            start_command: format!("npm run {}", script_names[selected]),
+            run_times: vec![],
+        })
+    } else {
+        panic!("scripts property is not an object");
+    }
+}
+
 fn main() {
     let mut config = Config::load_config();
-    let server = pick_server(&mut config);
 
-    // Record another run on this server
-    server.run_times.push(
-        SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards")
-            .as_millis(),
-    );
+    let matches = App::new("server-room")
+        .version("0.1.0")
+        .author("Caleb Cox")
+        .about("Runs dev servers")
+        .subcommand(
+            SubCommand::with_name("add")
+                .about("add a new server")
+                .arg(
+                    Arg::with_name("project-name")
+                        .help("Identifies the project name")
+                        .takes_value(true)
+                        .short("p")
+                        .long("project-name")
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name("start-script")
+                        .help("Sets the new server's start script")
+                        .takes_value(true)
+                        .short("s")
+                        .long("start-script"),
+                ),
+        )
+        .subcommand(SubCommand::with_name("run").about("run a server"))
+        .get_matches();
 
-    config.flush_config();
+    match matches.subcommand_name() {
+        Some("add") => {
+            add_server(
+                &mut config,
+                matches
+                    .subcommand_matches("add")
+                    .unwrap()
+                    .value_of("project-name")
+                    .unwrap(),
+            );
+            config.flush_config();
+        }
+        Some("run") => {
+            // Record another run on this server
+            let server = pick_server(&mut config);
+            server.run_times.push(
+                SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .expect("Time went backwards")
+                    .as_millis(),
+            );
+            config.flush_config();
+        }
+        _ => println!("Some other subcommand was used"),
+    }
 }
