9b2ff7713000c94adabd0e9e3c0d3e09b0cca530
Caleb Cox <canac@users.noreply.github.com>
2021-09-12 00:10:03 -0500

    Validate command line args

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,7 @@
+use super::Script;
 use super::Server;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::fs;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -34,5 +36,75 @@ impl Config {
     pub fn add_server(self: &mut Config, server: Server) {
         self.servers.push(server);
         self.flush_config();
+    }
+
+    // Determine whether the project name refers to a valid new project
+    pub fn validate_new_project_name(self: &Config, project_name: &String) -> Result<(), String> {
+        match fs::metadata(format!(
+            "{}/{}/package.json",
+            self.servers_dir, project_name
+        )) {
+            Ok(metadata) => {
+                if !metadata.is_file() {
+                    return Err("Project package.json is not a file".to_string());
+                }
+
+                if self
+                    .servers
+                    .iter()
+                    .find(|server| &server.project_name == project_name)
+                    .is_some()
+                {
+                    return Err("Project already exists".to_string());
+                }
+            }
+            Err(_) => return Err("Project doesn't have a valid package.json".to_string()),
+        }
+
+        Ok(())
+    }
+
+    // Return a vector of the project's start scripts
+    pub fn load_project_start_scripts(
+        self: &Config,
+        project_name: &String,
+    ) -> Result<Vec<Script>, String> {
+        let package_json_path = format!("{}/{}/package.json", self.servers_dir, project_name);
+        let package_json_content = match fs::read_to_string(package_json_path) {
+            Ok(content) => content,
+            Err(_) => return Err("Error reading package.json".to_string()),
+        };
+        let package_json: Value = match serde_json::from_str(&package_json_content) {
+            Ok(json) => json,
+            Err(_) => return Err("Error parsing package.json".to_string()),
+        };
+        match &package_json["scripts"] {
+            Value::Object(scripts) => Ok(scripts
+                .iter()
+                .map(|(name, command)| Script {
+                    name: name.to_string(),
+                    command: command.to_string(),
+                })
+                .collect::<Vec<_>>()),
+            _ => return Err("scripts property is not an object".to_string()),
+        }
+    }
+
+    // Determine whether the start script for a project is valid
+    pub fn validate_start_script(
+        self: &Config,
+        project_name: &String,
+        start_script: &String,
+    ) -> Result<(), String> {
+        let scripts = self.load_project_start_scripts(project_name)?;
+        if scripts
+            .iter()
+            .find(|script| &script.name == start_script)
+            .is_none()
+        {
+            return Err("Start script doesn't exist".to_string());
+        }
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,6 @@ use server::Server;
 
 use clap::{App, Arg, SubCommand};
 use inquire::Select;
-use serde_json::Value;
-use std::collections::HashSet;
 use std::fs;
 
 // Let the user pick a server from the defined list in the config
@@ -21,28 +19,19 @@ fn pick_server(config: &mut Config) -> &mut Server {
 
 // Pick a new project from the servers directory that isn't a server yet
 fn pick_project(config: &Config) -> String {
-    let existing_projects: HashSet<_> = config
-        .servers
-        .iter()
-        .map(|server| &server.project_name)
-        .collect();
     let projects = fs::read_dir(&config.servers_dir)
         .expect("Error reading servers directory")
         .filter_map(|result| {
             if let Ok(dir_entry) = result {
-                // Keep this directory if it contains a package.json file and isn't already a server
-                if let Ok(metadata) = fs::metadata(format!(
-                    "{}/package.json",
-                    dir_entry.path().to_str().unwrap().to_string()
-                )) {
-                    let project_name = dir_entry.file_name().to_str().unwrap().to_string();
-                    if metadata.is_file() && !existing_projects.contains(&project_name) {
-                        return Some(project_name);
-                    }
+                let project_name = dir_entry.file_name().to_str().unwrap().to_string();
+                if config.validate_new_project_name(&project_name).is_ok() {
+                    Some(project_name)
+                } else {
+                    None
                 }
+            } else {
+                None
             }
-
-            None
         })
         .collect::<Vec<_>>();
     Select::new("Pick a project", projects).prompt().unwrap()
@@ -50,25 +39,12 @@ fn pick_project(config: &Config) -> String {
 
 // Pick a start script for a particular project
 fn pick_start_script(config: &Config, project_name: &String) -> Script {
-    let package_json_path = format!("{}/{}/package.json", config.servers_dir, project_name);
-    let package_json_content =
-        fs::read_to_string(package_json_path).expect("Error reading package.json");
-    let package_json: Value = serde_json::from_str(&package_json_content).unwrap();
-    match &package_json["scripts"] {
-        Value::Object(scripts_json) => {
-            let scripts = scripts_json
-                .iter()
-                .map(|(name, command)| Script {
-                    name: name.to_string(),
-                    command: command.to_string(),
-                })
-                .collect::<Vec<_>>();
-            Select::new("Pick a start command", scripts)
-                .prompt()
-                .unwrap()
-        }
-        _ => panic!("scripts property is not an object"),
-    }
+    Select::new(
+        "Pick a start command",
+        config.load_project_start_scripts(project_name).unwrap(),
+    )
+    .prompt()
+    .unwrap()
 }
 
 fn main() {
@@ -104,11 +80,27 @@ fn main() {
         Some("add") => {
             let options = matches.subcommand_matches("add").unwrap();
             let project_name = match options.value_of("project-name") {
-                Some(project_name) => project_name.to_string(),
+                Some(project_name) => {
+                    let project_name = project_name.to_string();
+                    if let Err(msg) = config.validate_new_project_name(&project_name) {
+                        eprint!("{}", msg);
+                        return;
+                    }
+
+                    project_name
+                }
                 None => pick_project(&config),
             };
             let start_script = match options.value_of("start-script") {
-                Some(start_script) => start_script.to_string(),
+                Some(start_script) => {
+                    let start_script = start_script.to_string();
+                    if let Err(msg) = config.validate_start_script(&project_name, &start_script) {
+                        eprint!("{}", msg);
+                        return;
+                    }
+
+                    start_script
+                }
                 None => format!("npm run {}", pick_start_script(&config, &project_name).name),
             };
             config.add_server(Server::new(project_name, start_script));
